fcdf40be788a9ef681cbda16a6cac6cd0a4a3490
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-11-04 17:43:25 -0500

    Adding feature to create new picture using HTML form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -42,4 +42,16 @@ class PicturesController < ApplicationController
   	end
 
 
+  	def new
+
+  	end
+
+
+  	def create
+  		    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+
+  	end
+
+
+
 end
